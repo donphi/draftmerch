a427627a1481a541b23e4fc040ef82d5603269bf
chonkie <philp.donald@gmail.com>
2024-01-28 13:59:19 +0000

    Lambda Function update

--- mysite/lambda_function_for_def_generate_image/lambda_function.py
+++ mysite/lambda_function_for_def_generate_image/lambda_function.py
@@ -23,6 +23,9 @@ def get_secret(secret_name):
 def lambda_handler(event, context):
     # Fetch the API key from Secrets Manager
     api_key = get_secret('Generator')
+    template_with_photo = get_secret('PromptTemplateWithPhoto')['promptTemplate']
+    template_without_photo = get_secret('PromptTemplateWithoutPhoto')['promptTemplate']
+    
     # Extract parameters from the event object, assuming it's a JSON payload
     # You may need to change this part to match the structure of the incoming event
     body = json.loads(event['body'])
@@ -41,20 +44,9 @@ def lambda_handler(event, context):
 
     # Construct the prompt depending on whether an uploaded image description is provided
     if uploaded_image_description:
-        final_prompt = ("Use the generation ID TXLnYeq6rsLqVTTa, uvpARKZcAUCKOBn7 and P3PUOoDqFsKHJXAA. Medium: 2D, "
-                        "shield-like shaped, very detailed sports vector emblem only. Subject: A character is engaged in {sport}, "
-                        "depicting intricate detail in the character wearing the {sport}'s attire and equipment. This character will "
-                        "be detailed at the end. The character is depicted {action}. Emotion: {personality}, displayed in detail on the character's face. Lighting: Flat, "
-                        "Vivid, and bold, and use striking {color} to accentuate key elements. White background. Scene: The emblem is "
-                        "surrounded by a pure white background, highlighted by a prominent, heavy black border. Style: Sharp, clean lines, "
-                        "bold use of {color}. The description of the character is {character}.").format(character=uploaded_image_description, personality=personality, sport=sport, color=color, action=action)
+        final_prompt = template_with_photo.format(character=uploaded_image_description, personality=personality, sport=sport, color=color, action=action)
     else:
-        final_prompt = ("Use the generation ID TXLnYeq6rsLqVTTa, uvpARKZcAUCKOBn7 and P3PUOoDqFsKHJXAA. Medium: 2D, shield-like shaped, "
-                        "very detailed sports vector emblem only. Subject: A {hero} mascot for {sport}, depicting intricate detail in the "
-                        "{sport}'s attire and equipment. The {hero} is depicted {action}. Emotion: {personality}, displayed in detail on the "
-                        "{hero}'s face. Lighting: Flat, Vivid, and bold, and use striking {color} to accentuate key elements. White background. "
-                        "Scene: The emblem is surrounded by a pure white background, highlighted by a prominent, heavy black border. Style: Sharp, "
-                        "clean lines, bold use of {color}. --ar 1:1").format(hero=hero, action=action, personality=personality, sport=sport, color=color)
+        final_prompt = template_without_photo.format(hero=hero, action=action, personality=personality, sport=sport, color=color)
 
     # Log the generated prompt on the server side
     logging.info(f"Generated Prompt: {final_prompt}")
