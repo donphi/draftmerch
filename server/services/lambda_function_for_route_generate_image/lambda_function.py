ded782443967f648fee9fbdfee589e23653c94cb
chonkie <philp.donald@gmail.com>
2024-02-10 10:01:38 +0000

    lambda update

--- server/services/lambda_function_for_route_generate_image/lambda_function.py
+++ server/services/lambda_function_for_route_generate_image/lambda_function.py
@@ -18,21 +18,17 @@ s3_client = boto3.client('s3')
 bucket_name = 'draft-images-bucket'
 
 def upload_to_s3(bucket, key, image):
-    # Save image in BytesIO
     buffer = BytesIO()
     image.save(buffer, format='PNG')
-    buffer.seek(0)  # Move the cursor to the beginning of the file
+    buffer.seek(0)  # Reset buffer position
 
     # Upload to S3
     s3_client.upload_fileobj(buffer, bucket, key, ExtraArgs={'ContentType': 'image/png'})
-
-    # Return the URL for the uploaded image
     return f"https://{bucket}.s3.amazonaws.com/{key}"
 
 def lambda_handler(event, context):
     logger.info(f'Event: {event}')
 
-    # Initializing response headers
     headers = {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
@@ -41,23 +37,17 @@ def lambda_handler(event, context):
     }
 
     try:
-        if event['httpMethod'] == 'OPTIONS':
-            return {
-                'statusCode': 200,
-                'headers': headers,
-                'body': json.dumps({'message': 'CORS preflight response'})
-            }
+        # Adjusted to check for 'httpMethod' in event
+        http_method = event.get('httpMethod')
+        if http_method == 'OPTIONS':
+            return {'statusCode': 200, 'headers': headers, 'body': json.dumps({'message': 'CORS preflight response'})}
 
-        if event['httpMethod'] != 'POST':
-            return {
-                'statusCode': 405,
-                'headers': headers,
-                'body': json.dumps({'error': 'Method not allowed'})
-            }
+        if http_method and http_method != 'POST':
+            return {'statusCode': 405, 'headers': headers, 'body': json.dumps({'error': 'Method not allowed'})}
 
-        body = json.loads(event['body'])
-        logger.info(f'Body after JSON deserialization: {body}')
-
+        # Direct Lambda invocation adaptation
+        body = event if not http_method else json.loads(event['body'])
+        
         response = lambda_client.invoke(
             FunctionName='gen_ima',
             InvocationType='RequestResponse',
@@ -70,8 +60,7 @@ def lambda_handler(event, context):
         if 'statusCode' in response_payload and response_payload['statusCode'] == 200:
             gen_ima_result = json.loads(response_payload['body'])
             image_url = gen_ima_result.get('image_url')
-            logger.info(f'Image URL: {image_url}')
-
+            
             response = requests.get(image_url)
             response.raise_for_status()
 
@@ -80,16 +69,13 @@ def lambda_handler(event, context):
             original_image_key = f"image_original/{filename}"
             watermarked_image_key = f"watermarked_image/(Watermark) {filename}"
 
-            # Upload the original image to S3
             original_image_url = upload_to_s3(bucket_name, original_image_key, original_image)
 
-            # Get the watermark image from S3
             watermark_image_key = "watermark/watermark.png"
             watermark_response = s3_client.get_object(Bucket=bucket_name, Key=watermark_image_key)
             watermark_image = Image.open(watermark_response['Body']).convert('RGBA')
             watermark_image = watermark_image.resize(original_image.size, Image.LANCZOS)
 
-            # Apply the watermark to the original image and upload to S3
             watermarked_image = Image.alpha_composite(original_image, watermark_image).convert('RGB')
             watermarked_image_url = upload_to_s3(bucket_name, watermarked_image_key, watermarked_image)
 
@@ -112,11 +98,7 @@ def lambda_handler(event, context):
 
     except Exception as e:
         logger.exception(f"Exception occurred: {e}")
-        return {
-            'statusCode': 500,
-            'headers': headers,
-            'body': json.dumps({'error': str(e)})
-        }
+        return {'statusCode': 500, 'headers': headers, 'body': json.dumps({'error': str(e)})}
 
 def formatted_filename(hero, personality, sport, color, action):
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
