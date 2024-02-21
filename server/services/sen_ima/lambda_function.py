d3fc40e33a29c2a88a35f02e37a84e605bde1966
chonkie <philp.donald@gmail.com>
2024-02-21 15:41:51 +0000

    lambda update

--- server/services/sen_ima/lambda_function.py
+++ server/services/sen_ima/lambda_function.py
@@ -1,5 +1,9 @@
 import json
 import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Assuming you have the same API Gateway setup as Lambda A
 api_gw_client = boto3.client('apigatewaymanagementapi', endpoint_url='https://0pgyxaha81.execute-api.us-east-1.amazonaws.com/prod/')
@@ -21,14 +25,19 @@ def lambda_handler(event, context):
         'connectionId': connection_id
     }
 
+    logger.info(f"Processing with connectionId: {connection_id} and renderId: {render_id}")
+
     try:
+        logger.info(f"Sending message to connectionId: {connection_id}")
         api_gw_client.post_to_connection(
             ConnectionId=connection_id,
             Data=json.dumps(message)
         )
         return {'statusCode': 200, 'body': json.dumps({'success': True})}
-    except api_gw_client.exceptions.GoneException:
+    except api_gw_client.exceptions.GoneException as e:
+        logger.error(f"Connection gone error: {str(e)}")
         # Handling cases where the connection ID is no longer valid
         return {'statusCode': 410, 'body': json.dumps({'error': 'Connection gone'})}
     except Exception as e:
+        logger.error(f"General exception: {str(e)}")
         return {'statusCode': 500, 'body': json.dumps({'error': str(e)})}
